2f9796ec1d0096c4dbfe0c68316a367b50ccffbe
Kayla <kln47@cornell.edu>
2024-06-21 21:56:05 -0400

    fixed mongoose error

--- server/index.js
+++ server/index.js
@@ -1,24 +1,47 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
 
-dotenv.config();
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    collection = client.db("EduNotes").collection("userData");
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+}
+const mongoURI = process.env.MONGO_URL
+const PORT = process.env.PORT;
+console.log("HERE")
+console.log(mongoURI)
+console.log(PORT)
 
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const client = new MongoClient(mongoURI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function startServer() {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+startServer()
