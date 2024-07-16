145fa362e61279b1c3349c9edb1e95d882f3f436
Kayla <pandakay88@gmail.com>
2024-07-16 12:52:22 -0400

    Updates - implemented responsiveness for grid - added add + sort icons into table-header

--- client/src/routes/listings.js
+++ client/src/routes/listings.js
@@ -1,15 +1,24 @@
 import './listings.css';
-import Item from '../Components/item'
+import Item from '../Components/item';
+import addIcon from '../Media/add.png';
+import sortIcon from '../Media/sort.png';
 
 function Listings() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className = "table-header">
-          <input placeholder='search'></input>
+        <div className="table-header">
+          <img src={addIcon} alt="add icon" />
+          <input 
+            placeholder="search"
+            type="text" 
+            // value={searchInput}
+            // onChange={}
+          />
+          <img src={sortIcon} alt="sort icon" />
         </div>
-        <div className = "table">
-          <Item/>
+        <div className="table">
+          <Item />
         </div>
       </header>
     </div>
